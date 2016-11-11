afb551a79842b521f126c175a417f44f27d39898
Ari_tan <1690985@qq.com>
2016-11-11 15:15:34 +0800

    myshop0.0.01

---
+++ Application/Common/Conf/config.php
@@ -0,0 +1,22 @@
+<?php
+return array(
+	'TMPL_L_DELIM'          =>  '<{',            // æ¨¡æ¿å¼•æ“Žæ™®é€šæ ‡ç­¾å¼€å§‹æ ‡è®?
+	'TMPL_R_DELIM'          =>  '}>',
+
+	'DB_TYPE'               =>  'mysql',     // æ•°æ®åº“ç±»åž?
+	'DB_HOST'               =>  'localhost', // æœåŠ¡å™¨åœ°å€
+	'DB_NAME'               =>  'myshop',          // æ•°æ®åº“å
+	'DB_USER'               =>  'root',      // ç”¨æˆ·å?
+	'DB_PWD'                =>  '',          // å¯†ç 
+	'DB_PORT'   			=> 	3306, // ç«¯å£
+	'DB_PREFIX' 			=> 	'ms_', // æ•°æ®åº“è¡¨å‰ç¼€ 
+	'DB_CHARSET'			=> 	'utf8', // å­—ç¬¦é›?
+
+	'TMPL_FILE_DEPR'        =>  '_',	//ç®€åŒ–æ¨¡æ?
+
+	'URL_MODEL'				=>  2,		//ç¬¬äºŒç§URLæ–¹æ³•
+
+	'ERROR_PAGE'			=>	'http://localhost/404.html',
+
+	'SESSION_TYPE'  =>  'Db',			//session æ•°æ®åº?
+);
---
+++ Application/Runtime/Cache/Home/2a94b8bda04ff446dcedd42276fc99ee.php
@@ -0,0 +1,15 @@
+<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html>
+<html lang="zh-CN">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Easy Shop</title>
+    <link href="/web/Public/css/bootstrap.min.css" rel="stylesheet">
+    <script src="/web/Public/js/jquery.min.js"></script>
+    <script src="/web/Public/js/bootstrap.min.js"></script>
+  </head>
+  <body>
+    <h1>你好，世界！</h1>
+  </body>
+</html>
