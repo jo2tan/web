d7e30401724cdbe762944207e92fb490e23cc317
Ari_tan <1690985@qq.com>
2017-02-14 15:38:28 +0800

    10.10.10

--- Application/Runtime/Cache/Home/0d6509e863112dc5821b22ecb897e16e.php
+++ Application/Runtime/Cache/Home/0d6509e863112dc5821b22ecb897e16e.php
@@ -1,1 +1,23 @@
-<?php if (!defined('THINK_PATH')) exit();?>
+<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html>
+<html lang="zh-CN">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>My web</title>
+    <link href="/web/Public/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/web/Public/css/home.css" rel="stylesheet">
+
+    <script src="/web/Public/js/jquery.min.js"></script>
+    <script src="/web/Public/js/bootstrap.min.js"></script>
+    <script src="/web/Public/js/home.js"></script>
+  </head>
+  <body>
+
+    <h1>123123</h1>
+
+
+
+
+  </body>
+</html>
